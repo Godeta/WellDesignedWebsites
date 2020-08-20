ce89673dfd62d01121c766ea38edab758d1e38f5
Godeta <Godeta61@gmail.com>
2020-08-21 01:57:28 +0200

    changement langage enfin compris

--- bootstrapResponsive/languages.js
+++ bootstrapResponsive/languages.js
@@ -1,23 +1,33 @@
 var data = {
+	// attention ! Pour que cela fonctionne il faut que dans l'ordre du html cela corresponde donc : title1 puis description puis subText...
 			  "english": 
 			  {
 			    "title1": "Welcome",
-			    "description": 
-                    "Truc.",
-                "subText": "Chosen language : English."
+			    "description": "Truc.",
+				"subText2": "Chosen language : English.",
+				"voirDemo": 
+					"View Demo",
+					"Test": 
+						"Truc."
 			  },
 			  "french": 
 			  {
 			    "title1": "Bienvenu",
-			    "description": 
-				    "Artun.",
-                "subText": "Langage choisi : français"
+			    "description": "Artun.",
+				"subText2": "Langage choisi : français",
+				"voirDemo":
+					 "Voir la demo",
+					 "Test": 
+						 "Truc."
 			  },
 			  "spanish": 
 			  {
 			    "title1": "Hola como estas ?",
-			    "description": 
-				    "Siiiii me gustaaa (:",
-                "subText": "Idioma eligen : espanol"
+			    "description": "Siiiii me gustaaa (:",
+				"subText2": "Idioma eligen : espanol",
+				"voirDemo": 
+					"Mirar la demo",
+					"Test": 
+						"Truc."
 			  }
 			}
