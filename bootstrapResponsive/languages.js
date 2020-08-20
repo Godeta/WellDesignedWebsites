3f43e03eadc0e5301b7db507173d6c09f6d3e5c1
Godeta <Godeta61@gmail.com>
2020-08-20 02:43:03 +0200

    Possibilité changement de langue en cours

---
+++ bootstrapResponsive/languages.js
@@ -0,0 +1,23 @@
+var data = {
+			  "english": 
+			  {
+			    "title1": "Welcome",
+			    "description": 
+                    "Truc.",
+                "subText": "Chosen language : English."
+			  },
+			  "french": 
+			  {
+			    "title1": "Bienvenu",
+			    "description": 
+				    "Artun.",
+                "subText": "Langage choisi : français"
+			  },
+			  "spanish": 
+			  {
+			    "title1": "Hola como estas ?",
+			    "description": 
+				    "Siiiii me gustaaa (:",
+                "subText": "Idioma eligen : espanol"
+			  }
+			}
