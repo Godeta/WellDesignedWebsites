3be9c0d1b5ad1a5d67c15978f2ba521e468008c6
Godeta <Godeta61@gmail.com>
2021-04-26 23:49:42 +0200

    début des modifications pour réaliser le projet ECADNET

---
+++ ECADNET/languages.js
@@ -0,0 +1,33 @@
+var data = {
+	// attention ! Pour que cela fonctionne il faut que dans l'ordre du html cela corresponde donc : title1 puis description puis subText...
+			  "english": 
+			  {
+			    "title1": "Welcome",
+			    "description": "Something.",
+				"subText2": "Chosen language : English.",
+				"voirDemo": 
+					"View Demo",
+					"Test": 
+						"Stuff."
+			  },
+			  "french": 
+			  {
+			    "title1": "Bienvenu",
+			    "description": "Descrip en français.",
+				"subText2": "Langage choisi : français",
+				"voirDemo":
+					 "Voir la demo",
+					 "Test": 
+						 "Truc."
+			  },
+			  "spanish": 
+			  {
+			    "title1": "Hola como estas ?",
+			    "description": "Siiiii me gustaaa (:",
+				"subText2": "Idioma eligen : espanol",
+				"voirDemo": 
+					"Mirar la demo",
+					"Test": 
+						"Cosa."
+			  }
+			}
